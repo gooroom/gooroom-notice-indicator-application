c3ae9c0856752595146ff481d435c14052431e4f
donghun <donghun@gooroom.kr>
2019-11-29 16:06:58 +0900

    Update sources regarding copyright
    Change-Id: I1127969a70618ece4333a1e7af3e7e253d102ad1

--- src/gooroom-notice-applet.h
+++ src/gooroom-notice-applet.h
@@ -1,9 +1,8 @@
 /*
+ * Copyright (C) 2018-2019 Gooroom <gooroom@gooroom.kr>
  *
- * Copyright (C) 2015-2019 Gooroom <gooroom@gooroom.kr>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
  * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
  *
@@ -13,8 +12,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- *
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
  */
 
 #ifndef __GOOROOM_NOTICE_APPLET_H__
